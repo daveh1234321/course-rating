8528a2002960375084874b444a4aa23034c15f9c
Dave H <bitbucket@daveh.xyz>
2020-04-17 10:41:44 +0100

    Removed alerts from queries

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -5,13 +5,11 @@ import * as mutations from '../graphql/mutations';
 export const courseMutation = async (course) => {
     console.log(course);
     const newCourse = await API.graphql(graphqlOperation(mutations.createCourse, {input: course}));
-    alert(JSON.stringify(newCourse));
     return newCourse;
 };
   
   
 export const listCourses = async () => {
     const allCourses = await API.graphql(graphqlOperation(queries.listCourses));
-    alert(JSON.stringify(allCourses.data.listCourses.items));
     return allCourses.data.listCourses.items;
 };
